0981b8629ae3067136ab0b5a062866aea7d04513
sofiachiare <sofia.chiarello1999@gmail.com>
2020-12-02 16:43:20 +0100

    close #2 correzione

--- src/main/java/it/unipd/tos/business/Ordinazione.java
+++ src/main/java/it/unipd/tos/business/Ordinazione.java
@@ -6,6 +6,7 @@ package it.unipd.tos.business;
 import java.util.List;
 
 import it.unipd.tos.business.exception.TakeAwayBillException;
+import it.unipd.tos.model.Categoria;
 import it.unipd.tos.model.MenuItem;
 
 public class Ordinazione implements TakeAwayBill {
@@ -13,29 +14,24 @@ public class Ordinazione implements TakeAwayBill {
 @Override
 public double getOrderPrice(List<MenuItem> itemsOrdered, User user) throws TakeAwayBillException{
 double totale =0;
-MenuItem min = itemsOrdered.get(0);
-if(itemsOrdered.size()>=5) {
+int contaGelati = 0;
 for(MenuItem a : itemsOrdered) {
-if (a.getPrezzo() < min.getPrezzo()) {
-min=a;
+totale+=a.getPrezzo();
+if (a.itemType==Categoria.Gelati) {
+contaGelati++;
 }
 }
-for(MenuItem a : itemsOrdered) {
-if(a==min) {
-totale += (a.getPrezzo())/2;
+MenuItem min = new MenuItem(Categoria.Gelati, "prova", 9999999);
+if (contaGelati>=5){
+for(MenuItem b : itemsOrdered) {
+if(b.itemType==Categoria.Gelati) {
+if(b.getPrezzo()<min.getPrezzo()) {
+min=b;
+} 
 }
-else {
-totale += a.getPrezzo();
 }
-}
-}
-else {
-for(MenuItem a : itemsOrdered) {
-totale += a.getPrezzo();
-}
+totale=totale-(min.getPrezzo()/2);
 }
 return totale;
 }
-
 }
-
