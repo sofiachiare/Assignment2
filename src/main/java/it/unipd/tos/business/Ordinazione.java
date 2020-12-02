08596eb3bd9d87de2328afc38624df7bbcd945f0
sofiachiare <sofia.chiarello1999@gmail.com>
2020-12-02 15:53:27 +0100

    close #2

--- src/main/java/it/unipd/tos/business/Ordinazione.java
+++ src/main/java/it/unipd/tos/business/Ordinazione.java
@@ -13,9 +13,29 @@ public class Ordinazione implements TakeAwayBill {
 @Override
 public double getOrderPrice(List<MenuItem> itemsOrdered, User user) throws TakeAwayBillException{
 double totale =0;
+MenuItem min = itemsOrdered.get(0);
+if(itemsOrdered.size()>=5) {
 for(MenuItem a : itemsOrdered) {
-totale += a.getPrezzo();}
+if (a.getPrezzo() < min.getPrezzo()) {
+min=a;
+}
+}
+for(MenuItem a : itemsOrdered) {
+if(a==min) {
+totale += (a.getPrezzo())/2;
+}
+else {
+totale += a.getPrezzo();
+}
+}
+}
+else {
+for(MenuItem a : itemsOrdered) {
+totale += a.getPrezzo();
+}
+}
 return totale;
 }
 
 }
+
